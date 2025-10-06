6c59bb613a884ce1b09e86603538150eb333e96d
Andrei Dodu <andrei.dodu@hotmail.it>
2025-10-06 04:42:04 +0200

    wip

--- src/main/java/com/andreidodu/virtualthreads/MainYield.java
+++ src/main/java/com/andreidodu/virtualthreads/MainYield.java
@@ -3,15 +3,18 @@ package com.andreidodu.virtualthreads;
 import com.andreidodu.virtualthreads.task.TaskRunnable;
 
 import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
 
 public class MainYield {
 
     public static void main(String[] args) throws InterruptedException {
-        var t1 = Thread.ofVirtual().name("t1").unstarted(new TaskRunnable(1));
-        var t2 = Thread.ofVirtual().name("t2").unstarted(new TaskRunnable(2));
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        var t1 = Thread.ofVirtual().name("t1").unstarted(new TaskRunnable(1, countDownLatch));
+        var t2 = Thread.ofVirtual().name("t2").unstarted(new TaskRunnable(2, countDownLatch));
 
         t1.start();
         t2.start();
-        Thread.sleep(Duration.ofSeconds(Integer.MAX_VALUE));
+        countDownLatch.await();
     }
 }
--- src/main/java/com/andreidodu/virtualthreads/task/TaskRunnable.java
+++ src/main/java/com/andreidodu/virtualthreads/task/TaskRunnable.java
@@ -1,8 +1,9 @@
 package com.andreidodu.virtualthreads.task;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.stream.IntStream;
 
-public record TaskRunnable(int id) implements Runnable {
+public record TaskRunnable(int id, CountDownLatch countDownLatch) implements Runnable {
 
     @Override
     public void run() {
@@ -10,5 +11,6 @@ public record TaskRunnable(int id) implements Runnable {
             System.out.println("task " + id + "on thread " + Thread.currentThread().getName());
             Thread.yield();
         });
+        countDownLatch.countDown();
     }
 }
